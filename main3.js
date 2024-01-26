f7c09ce8b17926232c09bb010f3e148bbb54c7e1
Sudarshan Adhikari <sdadkry95@gmail.com>
2024-01-26 13:59:58 +0545

    added object and array

---
+++ main3.js
@@ -0,0 +1,28 @@
+const student = {
+    name: "Simran",
+    address: "Thachaina",
+    phone: "1234567890",
+};
+const student2 = {
+    name: "Suraj",
+    address: "Thachaina",
+    phone: "1234567890",
+};
+
+/// array
+
+const arr = [1, 2, 3, 4];
+const arr2 = ["1", "2", 3, 4];
+const arr3 = [{
+        name: "Simran",
+        address: "Thachaina",
+        phone: "1234567890",
+    },
+    {
+        name: "Simran",
+        address: "Thachaina",
+        phone: "1234567890",
+    },
+];
+
+//array object
