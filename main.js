8a5d045bf777808e7a1e41525b23e93841c024ba
Sudarshan Adhikari <sdadkry95@gmail.com>
2024-01-26 13:54:47 +0545

    basic js

---
+++ main.js
@@ -0,0 +1,17 @@
+//const
+const a = 5;
+
+//let
+
+let b = "6";
+
+b = "adsad";
+
+//var
+
+var c = 7;
+
+var c = 9;
+
+
+// console.log(c)
---
+++ main2.js
@@ -0,0 +1,7 @@
+const first = 200;
+
+const second = Number("400");
+
+const sum = String(first) + Number(second);
+
+console.log(sum);
